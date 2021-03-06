97e84b9c200fb99fee38f5b5c5d220c8eec181bd
Niraaxx <60941711+Niraaxx@users.noreply.github.com>
2021-03-06 20:46:37 +0100

    Add files via upload

---
+++ ChallengeMe/ChallengeMe/Niveau2.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChallengeMe
+{
+    /// <summary>
+    /// Logique d'interaction pour Niveau2.xaml  --> niveau encore pas implémenter
+    /// </summary>
+    public partial class Niveau2 : Window
+    {
+        private String name;
+        private int score;
+
+        public Niveau2(string name,int score)
+        {
+            InitializeComponent();
+            this.name = name;
+            this.score = score;
+        }
+
+        public int Score { get => score; set => score = value; }
+
+        private void pseudoAfficher(object sender, RoutedEventArgs e)
+        {
+            this.pseudo.Content = this.name;
+        }
+
+        private void scoreAfficher(object sender, RoutedEventArgs e)
+        {
+            this.scoring.Content = Convert.ToString(Score);
+        }
+
+        private void scoreUpdate(object sender, DataTransferEventArgs e)
+        {
+            this.score = score + 1;
+            this.scoreAfficher(sender, e);
+        }
+
+        private void changerNiveau(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            this.score = score + 1;
+            Niveau3 p = new Niveau3(name, score);
+            p.ShowDialog();
+        }
+    }
+}
