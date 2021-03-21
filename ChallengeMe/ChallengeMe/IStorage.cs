ea282e8a75152a2a762b91b6e6022126d7c64d32
Niraaxx <60941711+Niraaxx@users.noreply.github.com>
2021-03-21 18:20:03 +0100

    Premier pas save

---
+++ ChallengeMe/ChallengeMe/IStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeMe
+{
+    public interface IStorage
+    {
+        void Save(Joueur j);
+
+        Joueur Load();
+    }
+}
---
+++ ChallengeMe/ChallengeMe/JsonStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeMe
+{
+    public class JsonStorage : IStorage
+    {
+        private String file;
+
+        public Joueur Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Joueur j)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
