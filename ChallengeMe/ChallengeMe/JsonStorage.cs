a8e5e783beeafa6dd2fc09b3153049ced7891661
Niraaxx <60941711+Niraaxx@users.noreply.github.com>
2021-03-21 18:29:11 +0100

    Update JsonStorage.cs

--- ChallengeMe/ChallengeMe/JsonStorage.cs
+++ ChallengeMe/ChallengeMe/JsonStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,44 @@ namespace ChallengeMe
     public class JsonStorage : IStorage
     {
         private String file;
+        private Joueur joueur;
 
+        public JsonStorage(String file)
+        {
+            this.file = file;
+        }
         public Joueur Load()
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (FileStream flux = new FileStream(file, FileMode.Open))
+                {
+                    System.Runtime.Serialization.Json.DataContractJsonSerializer ser = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(Joueur));
+                    joueur = ser.ReadObject(flux) as Joueur;
+                }
+            }
+            catch
+            {
+                joueur = new Joueur();
+            }
+            return joueur;
         }
 
         public void Save(Joueur j)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (FileStream flux = new FileStream(file, FileMode.Create))
+                {
+                    System.Runtime.Serialization.Json.DataContractJsonSerializer ser = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(Joueur));
+                    ser.WriteObject(flux, joueur);
+                    
+                }
+            }
+            catch
+            {
+                new Exception("Erreur de la sauvegarde");
+            }
         }
     }
 }
